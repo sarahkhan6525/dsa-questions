ae55c53422ff3a370b525363a0ee9d711e0f6629
sarahkhan6525 <saarahkhan2024@gmail.com>
2024-02-11 13:09:18 +0530

    Even or Odd

---
+++ Basics/even_or_odd.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    if(num%2==0){
+        cout<<"Even"<<endl;
+    }
+    else{
+        cout<<"Odd"<<endl;
+    }
+    return 0;
+}
