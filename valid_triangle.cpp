702d93fa71bba23b47a22b4212ad138febf3bd5b
sarahkhan6525 <saarahkhan2024@gmail.com>
2024-02-11 13:28:24 +0530

    Valid Triangle

---
+++ valid_triangle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b>c&&b+c>a&&c+a>b){
+        cout<<"Valid Triangle"<<endl;
+    }
+    else{
+        cout<<"Invalid Triangle"<<endl;
+    }
+    return 0;
+}
