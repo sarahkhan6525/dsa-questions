3d2354e03a66cd24351051345aec4e5399fe0428
sarahkhan6525 <saarahkhan2024@gmail.com>
2024-08-16 16:35:46 +0530

    changes

--- Basics/Arrays/selection_sort.cpp
+++ Basics/Arrays/selection_sort.cpp
@@ -28,3 +28,18 @@ int main() {
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+void selection_sort(int arr[],int n){
+    for(int i=0;i<=n-2;i++){
+        int min =i;
+        for(int j=i+1;j<=n-1;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+}
